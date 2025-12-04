5bc7ab97d94437a4f71baac0ea94366cbfb78f38
Will Han <han00614@csel-cuda-01.cselabs.umn.edu>
2025-12-04 15:54:54 -0600

    updated file names and directory structure

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gcc -O3 serial_version.c -o serial_version -lm
+#gcc -O3 serial_version.c -o serial_version -lm
+nvcc cu_files/serial.cu -o serial
--- test_serial.sh
+++ test_serial.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./serial_version ./tests/correctness/testin0_mass.txt ./tests/correctness/testin0_coordinate.txt acceleration_0.txt 
+./serial ./tests/correctness/testin0_mass.txt ./tests/correctness/testin0_coordinate.txt acceleration_0.txt 
